ea3299315026b538d30471d1726a26695740c19a
shinnji <ec2-user@ip-172-31-21-242.ec2.internal>
2021-09-10 08:23:28 +0000

    finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b35d3213bab6f8f2f7231b8e21d614b9ce27730e15f311a2248d2663f8b0d95b62f2427df7b592f85763e8957938e0d496336375845c154d36d2bdcba606ce57'
